61b6372da1c3819c93f7a2a84f8c09d847f590b5
YannickBihege <yannickbtheone@hotmail.com>
2023-11-28 13:28:00 +0100

    Add annotation enableglobal to login controller

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 @Controller
 public class LoginController {
 
